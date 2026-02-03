c8ba40002573a017be920b86b998b359c95390f3
unknown <syamilhafizin97@gmail.com>
2026-02-03 12:54:20 +0800

    first commit

---
+++ PY/01_FirstCoding.py
@@ -0,0 +1,32 @@
+# name = "Syamil" 
+# age = 10
+# weight = 20.5
+# is_female = False 
+
+# print(name)
+# print(type(name))
+# print(age)
+# print(type(age))
+# print(weight)
+# print(type(weight))
+# print(is_female)
+# print(type(is_female))
+
+# x = 10
+# y = 3
+
+# print(x + y)     # Addition
+# print(x - y)     # Subtraction
+# print(x * y)     # Multiplication
+# print(x / y)     # Division
+# print(x // y)    # Floor Division
+# print(x % y)     # Modulus
+# print(x ** y)    # Exponentiation
+
+Celcius = 25
+Farenheit = (Celcius * 9/5) + 32
+
+print(Farenheit)
+print(type(Farenheit))
+
+
